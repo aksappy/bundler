799c6b40796fdb499d2afa7be3acfd4a37c33d64
bleakwizz <bleakyvoice@yahoo.in>
2023-05-29 20:59:58 -0400

    test

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,22 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name="chatgpt.api.key")
+    String apiKey;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        System.out.println("apiKey = " + apiKey);
         return "Hello from RESTEasy Reactive";
     }
 }
